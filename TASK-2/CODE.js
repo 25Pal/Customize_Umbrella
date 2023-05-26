63e2adc2149677a8ef186e42ca00651024e37bf9
25pal <pallaviuttekar333@gmail.com>
2023-05-26 17:30:36 +0530

    Task-2 Completed

--- TASK-2/CODE.js
+++ TASK-2/CODE.js
@@ -1,29 +1,17 @@
-// function Solve(arr) {
-//     let n = arr.length, cnt = arr[0], ans = 1;
-//     for (let i = 1; i < n; i++) {
-//         if (cnt == 0) {
-//             return -1;
-//         }
-//         cnt--;
-//         cnt = Math.max(cnt, arr[i]);
-//         if (i == n - 1) {
-//             break;
-//         }
-//         if (cnt == 0) {
-//             cnt = arr[i];
-//             ans++;
-//         }
-//     }
-//     return ans;
-// }
+//I have used Dynamic programming Approach 
 
 function Solve(arr) {
     const n = arr.length;
-    const dp = new Array(n).fill(Number.MAX_VALUE);
-    dp[0] = 0;
+    
+    const dp = new Array(n).fill(Number.MAX_VALUE); // Here i have created array of n length and i have filled all array values with max values.
+   
+    dp[0] = 0;                                      //Here i have initilized starting value of array with '0' 
+   
+//Now in for loop will check next values directly with a privious value in dp array 
     for (let i = 0; i < n; i++) {
-        for (let j = i + 1; j <= i + arr[i] && j < n; j++) {
-            dp[j] = Math.min(dp[j], dp[i] + 1);
+        for (let j = i + 1; j <= i + arr[i] && j < n; j++) { 
+            dp[j] = Math.min(dp[j], dp[i] + 1); 
+            
         }
     }
     if (dp[n - 1] == Number.MAX_VALUE) {
@@ -34,5 +22,15 @@ function Solve(arr) {
 // const arr = [2, 1, 2, 3, 1];
 // console.log(Solve(arr));
 
+
 const arr = [2,1,2,3,1];
 console.log(Solve(arr));
+
+//Here is updated array of dp 
+/**
+[ 0, 1, 1, 1.7976931348623157e+308, 1.7976931348623157e+308 ]
+[ 0, 1, 1, 1.7976931348623157e+308, 1.7976931348623157e+308 ]
+[ 0, 1, 1, 2, 1.7976931348623157e+308 ]
+[ 0, 1, 1, 2, 2 ]
+[ 0, 1, 1, 2, 2 ]
+ */
